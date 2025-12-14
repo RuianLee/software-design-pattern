77bd419f2a677f04934d17d07bb31c18a624f5b0
ruian-lee <anli95092@gmail.com>
2025-12-14 19:00:41 +0800

    完成5b道館

--- 打球時間/道館五C/V1/.vscode/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        MainController mainController = new MainController(new Tank(), new Telecom(), new KeyBoard());
-        mainController.pressButton('f');
-    }
-}
--- 打球時間/道館五C/V1/.vscode/src/KeyBoard.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class KeyBoard {
-    private List<Button> buttons = new ArrayList();
-}
